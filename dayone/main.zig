8b73631437c0e660b543ddca44e28f984762f9c0
Mark <mark@rendlelabs.com>
2024-12-05 12:36:42 +0000

    Day One Part One

---
+++ dayone/main.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const ArrayList = std.ArrayList;
+
+fn readLine(reader: std.fs.File.Reader, buffer: []u8) !?[]const u8 {
+    const line = (try reader.readUntilDelimiterOrEof(
+        buffer,
+        '\n',
+    )) orelse return null;
+    if (@import("builtin").os.tag == .windows) {
+        return std.mem.trimRight(u8, line, "\r");
+    } else {
+        return line;
+    }
+}
+
+pub fn main() !void {
+    const alloc = std.heap.page_allocator;
+    var listA = ArrayList(i32).init(alloc);
+    var listB = ArrayList(i32).init(alloc);
+
+    const cwd = std.fs.cwd();
+
+    const file = try cwd.openFile("data.txt", .{ .mode = .read_only });
+    defer file.close();
+    const reader = file.reader();
+    var buffer: [100]u8 = undefined;
+    var line = try readLine(reader, &buffer);
+    while (line != null and line.?.len > 0) {
+        var it = std.mem.tokenize(u8, line.?, " ");
+        const sa = it.next().?;
+        const sb = it.next().?;
+        const a = try std.fmt.parseInt(i32, sa, 10);
+        const b = try std.fmt.parseInt(i32, sb, 10);
+        try listA.append(a);
+        try listB.append(b);
+        line = try readLine(reader, &buffer);
+    }
+
+    const as = listA.items;
+    const bs = listB.items;
+
+    std.mem.sort(i32, as, {}, comptime std.sort.asc(i32));
+    std.mem.sort(i32, bs, {}, comptime std.sort.asc(i32));
+
+    var total: u32 = 0;
+
+    for (as, bs) |a, b| {
+        total += @abs(a - b);
+    }
+    std.debug.print("\n\n{d}\n", .{total});
+}
